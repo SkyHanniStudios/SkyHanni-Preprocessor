9b1b0ba5dfb30ee8531d5f5e7cef7434e6145faa
Cal <cwolfson58@gmail.com>
2025-03-14 19:00:09 +1100

    logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
-    implementation("com.github.SkyHanniStudios:SkyHanni-Remap:e728a0ae7e")
+    implementation("com.github.SkyHanniStudios:SkyHanni-Remap:6c04dbdd4b")
     implementation("net.fabricmc:mapping-io:0.6.1")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
